eb910f0c76da1ee22d15422ef20441532d013654
Ingrid Jin <super10895@Ingrids-MacBook-Pro.local>
2017-06-07 14:21:10 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,16 +2,21 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
+var checkForMatch = function() {
 if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
+console.log("You found a match!");
 } else {
-	alert("Sorry, try again.");
+console.log("Sorry, try again.");
 }
+};
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+};
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
+
